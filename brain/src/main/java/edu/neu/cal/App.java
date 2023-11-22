be4e41a075f231609607a614c93594a99e917d16
JHan0220 <877653250@qq.com>
2023-11-21 20:15:43 -0800

    first commit

---
+++ brain/src/main/java/edu/neu/cal/App.java
@@ -0,0 +1,13 @@
+package edu.neu.cal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
